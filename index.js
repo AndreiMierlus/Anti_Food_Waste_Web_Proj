f5b66d3b36f0ea7ad10187c4ef0f464d8c36da62
Andrei <mierlusmaziluion21@stud.ase.ro>
2024-01-18 02:27:35 +0200

    3rd commit

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import env from 'dotenv';
+import cors from 'cors';
+import DB_Init from './entity/dbInit.js';
+import userRouter from './routes/UserRouter.js';
+import itemRouter from './routes/ItemRouter.js';
+import friendRequestRouter from './routes/FriendRequestRouter.js';
+import createDbRouter from './routes/createDBRouter.js';
+import itemClaimRouter from './routes/ItemClaimRouter';      
+import friendGroupRouter from './routes/FriendGroupRouter.js';
 
 env.config();
 
 let app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let port = process.env.PORT || 8001;
+DB_Init();
+
+app.use('/api', userRouter);
+app.use('/api', itemRouter);
+app.use('/api', friendRequestRouter);
+app.use('/api', createDbRouter);
+app.use('/api', itemClaimRouter);
+app.use('/api', friendGroupRouter);
+
+let port = process.env.PORT || 9000;
 app.listen(port);
-console.log('Server is running on port ' + port);
+console.log ('API is listening on port ' + port)
